a9d96e6b2b053b401a925e1ac2a0963bbee6e639
vanand <vanand@zeomega.com>
2016-05-13 12:38:08 +0530

    Adding new queries

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 /*Select Query*/
 
-/*List all the customers for whom the invoices were raised for a particular time period (start date time and end date time will be parameter to the query)*/
+/*List all the customers for whom the invoices were raised for a particular time period 
+(start date time and end date time will be parameter to the query)*/
 
 select 
     cu.customerid, 
@@ -18,7 +19,7 @@ where inv.invoicedate BETWEEN '2016-01-28' and  '2016-06-01'
 
 select 
     cu.customername,
-    (invd.quantity * invd.unitprice - invd.discount) as total_cost
+    sum(invd.unitprice) as total_cost
 from invoicedetails invd
     inner join invoices inv on inv.invoiceid = invd.invoiceid
     inner join customers cu on cu.customerid = inv.customerid
@@ -69,3 +70,27 @@ inner join invoices inv on inv.invoiceid = invd.invoiceid
 inner join deliveries dl on dl.deliveryid = inv.deliveryid
 
 where dl.CountryOrRegion = 'India'
+
+
+/*Listing the customers who does not have any invoices*/
+
+select 
+    cu.customerid
+from customers cu
+
+where cu.customerid not in(
+select 
+   inv.customerid 
+from invoices inv)
+
+/*List the total cost of all the invoices for all the customers*/
+/*Group by : helps to arrange identical/similar data into groups.*/
+
+select 
+  cu.customername, 
+  sum(invd.unitprice) as total_cost
+from invoicedetails invd
+    inner join invoices inv on inv.invoiceid = invd.invoiceid
+    inner join customers cu on cu.customerid = inv.customerid
+where cu.customerid
+group by cu.customername
